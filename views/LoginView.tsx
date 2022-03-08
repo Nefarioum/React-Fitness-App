29d6a4e18f6f367ee056d5dd0cddc17274bc2ff1
Nefarioum <nefarioum@outlook.com>
2022-03-08 23:29:47 +0000

    Imported TW and added SafeAreaView to LoginView

--- views/LoginView.tsx
+++ views/LoginView.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
 import React from 'react'
+import tw from 'tailwind-react-native-classnames'
 
 const LoginView = () => {
   return (
-    <View>
-      <Text>LoginView</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Text>Login</Text>
+      </View>
+    </SafeAreaView> 
   )
 }
 
