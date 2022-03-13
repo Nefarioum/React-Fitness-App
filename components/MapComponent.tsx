c1e702c5a1aac5f70fddd817a5fe75f969325b16
Nefarioum <nefarioum@outlook.com>
2022-03-13 03:41:35 +0000

    Combined Componenet to load in View and implemented empty map

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps'
+import { useTailwind } from 'tailwind-rn/dist';
 
 const MapComponent = () => {
+  const tailwind = useTailwind();
+
   return (
-    <View>
-      <Text>MapComponent</Text>
-    </View>
+    <MapView
+        style = {tailwind(`flex-1`)}
+        initialRegion={{
+          latitude: 50.743407570472336,
+          longitude: -1.898896997420026,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}>
+     </MapView>
   )
 }
 
--- views/MapView.tsx
+++ views/MapView.tsx
@@ -1,10 +1,34 @@
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
+import { useTailwind } from 'tailwind-rn/dist';
+import { fireAuth } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
+
+import MapComponent from '../components/MapComponent'
+
 const MapView = () => {
+  const tailwind = useTailwind();
+  const Navigation = useNavigation();
+
   return (
     <View>
-      <Text>MapView</Text>
+      <View style={tailwind(`h-1/2`)}>
+        <MapComponent />
+      </View>
+      <View style={tailwind(`h-1/2`)}>
+        <Text style={tailwind(`text-center pt-5 text-xl`)}>👋 Hey {(fireAuth.currentUser?.displayName === null ? fireAuth.currentUser?.email : fireAuth.currentUser?.displayName)}!</Text>
+        <Text style={tailwind(`text-center pt-3`)}>The following events are currently available to attend:</Text>
+
+        <View style={tailwind(`h-3/4 w-full flex-col justify-end items-center`)}>
+          <TouchableOpacity
+            onPress={() => Navigation.navigate('HomeView' as never)}
+            style={tailwind(`bg-purple-500 border-gray-500 rounded-full w-40`)}>
+                <Text style={tailwind(`text-lg p-2 text-white text-center`)}>🏠 Home</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
     </View>
   )
 }
