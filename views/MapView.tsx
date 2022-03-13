1e5c1ea5918739baf70933827501429a4bc68463
Nefarioum <nefarioum@outlook.com>
2022-03-13 03:03:54 +0000

    Added Map View and Map Component base files

---
+++ views/MapView.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const MapView = () => {
+  return (
+    <View>
+      <Text>MapView</Text>
+    </View>
+  )
+}
+
+export default MapView
